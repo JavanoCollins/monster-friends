3551d0ad17f4ce7ec1458cbb23ef5b6676f23ffc
Resume Website <jdcollins242@gmail.com>
2020-08-14 19:08:14 -0400

    Updates

---
+++ precache-manifest.f8e1dbf99e96a3d3362e326373ec295e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7dce7e6c5f479f75df85a6b6181a9d",
+    "url": "/monster-friends/index.html"
+  },
+  {
+    "revision": "286c820712f4c4d3e0e1",
+    "url": "/monster-friends/static/css/main.9aeae52f.chunk.css"
+  },
+  {
+    "revision": "769cd9a57f3c00361370",
+    "url": "/monster-friends/static/js/2.a424c5e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-friends/static/js/2.a424c5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286c820712f4c4d3e0e1",
+    "url": "/monster-friends/static/js/main.aa5b242b.chunk.js"
+  },
+  {
+    "revision": "69183d4527e50c669c85",
+    "url": "/monster-friends/static/js/runtime-main.138e667d.js"
+  }
+]);
